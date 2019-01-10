c9aeee6eaa517659ae7db971aa19a805180ee32b
Louis Cannatti <l.cannatti@tricentis.com>
2019-01-10 16:23:42 -0600

    update auth to check ips instead of sessions

--- includes/auth.php
+++ includes/auth.php
@@ -3,8 +3,7 @@ $authStatus=0;
 update_device_table();
 if(!sess_authenticated()){
     if(is_post_request() && isset($_POST['password'])){
-        #Fully aware storing this straight in the source is stupid, but the use case did not call for individual user accounts
-        #To set this password, use 
+        #To set this password, follow instructions in config.php
         if(password_verify($_POST['password'],PASS_HASH)){
             authenticate_sess();
             $authStatus=1;
@@ -24,23 +23,23 @@ if(!sess_authenticated()){
 function sess_authenticated(){
     global $db;
     $authTest=$db->prepare("SELECT * FROM devices WHERE SESS_ID=?;");
-    $authTest->bind_param('s',$_COOKIE["PHPSESSID"]);
+    $authTest->bind_param('s',$_SERVER['REMOTE_ADDR']);
     $authTest->execute();
-    $res=$authTest->get_result();
-    return $res->num_rows > 0;
+    $authTest->store_result();
+    return $authTest->num_rows > 0;
 }
 
 function authenticate_sess(){
     global $db;
     $authQ=$db->prepare("INSERT INTO devices VALUES (?,now());");
-    $authQ->bind_param("s",$_COOKIE['PHPSESSID']);
+    $authQ->bind_param("s",$_SERVER['REMOTE_ADDR']);
     $authQ->execute();
 }
 
 function reauth_sess(){
     global $db;
     $reauthQ=$db->prepare("UPDATE devices SET LAST_ACTIVE=now() WHERE SESS_ID=?");
-    $reauthQ->bind_param("s",$_COOKIE['PHPSESSID']);
+    $reauthQ->bind_param("s",$_SERVER['REMOTE_ADDR']);
     $reauthQ->execute();
 }
 
@@ -53,6 +52,6 @@ function update_device_table(){
 function end_sess(){
     global $db;
     $end=$db->prepare("DELETE FROM devices WHERE SESS_ID=?");
-    $end->bind_param("s",$_COOKIE['PHPSESSID']);
+    $end->bind_param("s",$_SERVER['REMOTE_ADDR']);
     $end->execute();
 }
